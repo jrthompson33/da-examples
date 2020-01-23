3ce78dbbb01ffeff75e21e678821e32303201b84
John Thompson <jrthompson@gatech.edu>
2020-01-23 14:04:13 -0500

    First commit

---
+++ 04_punishing_racism/data.js
@@ -0,0 +1,427 @@
+var dataByParent = [
+  {
+    "key": "1",
+    "values": [
+      {
+        "key": "white_men",
+        "values": [
+          {
+            "kid_q": 1,
+            "parent_q": 1,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.313
+          },
+          {
+            "kid_q": 2,
+            "parent_q": 1,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.229
+          },
+          {
+            "kid_q": 3,
+            "parent_q": 1,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.202
+          },
+          {
+            "kid_q": 4,
+            "parent_q": 1,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.159
+          },
+          {
+            "kid_q": 5,
+            "parent_q": 1,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.097
+          }
+        ]
+      },
+      {
+        "key": "black_men",
+        "values": [
+          {
+            "kid_q": 1,
+            "parent_q": 1,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.485
+          },
+          {
+            "kid_q": 2,
+            "parent_q": 1,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.279
+          },
+          {
+            "kid_q": 3,
+            "parent_q": 1,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.147
+          },
+          {
+            "kid_q": 4,
+            "parent_q": 1,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.064
+          },
+          {
+            "kid_q": 5,
+            "parent_q": 1,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.025
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "key": "2",
+    "values": [
+      {
+        "key": "white_men",
+        "values": [
+          {
+            "kid_q": 1,
+            "parent_q": 2,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.227
+          },
+          {
+            "kid_q": 2,
+            "parent_q": 2,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.204
+          },
+          {
+            "kid_q": 3,
+            "parent_q": 2,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.228
+          },
+          {
+            "kid_q": 4,
+            "parent_q": 2,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.206
+          },
+          {
+            "kid_q": 5,
+            "parent_q": 2,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.135
+          }
+        ]
+      },
+      {
+        "key": "black_men",
+        "values": [
+          {
+            "kid_q": 1,
+            "parent_q": 2,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.394
+          },
+          {
+            "kid_q": 2,
+            "parent_q": 2,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.283
+          },
+          {
+            "kid_q": 3,
+            "parent_q": 2,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.188
+          },
+          {
+            "kid_q": 4,
+            "parent_q": 2,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.093
+          },
+          {
+            "kid_q": 5,
+            "parent_q": 2,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.042
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "key": "3",
+    "values": [
+      {
+        "key": "white_men",
+        "values": [
+          {
+            "kid_q": 1,
+            "parent_q": 3,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.169
+          },
+          {
+            "kid_q": 2,
+            "parent_q": 3,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.172
+          },
+          {
+            "kid_q": 3,
+            "parent_q": 3,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.232
+          },
+          {
+            "kid_q": 4,
+            "parent_q": 3,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.247
+          },
+          {
+            "kid_q": 5,
+            "parent_q": 3,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.18
+          }
+        ]
+      },
+      {
+        "key": "black_men",
+        "values": [
+          {
+            "kid_q": 1,
+            "parent_q": 3,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.33
+          },
+          {
+            "kid_q": 2,
+            "parent_q": 3,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.264
+          },
+          {
+            "kid_q": 3,
+            "parent_q": 3,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.214
+          },
+          {
+            "kid_q": 4,
+            "parent_q": 3,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.126
+          },
+          {
+            "kid_q": 5,
+            "parent_q": 3,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.067
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "key": "4",
+    "values": [
+      {
+        "key": "white_men",
+        "values": [
+          {
+            "kid_q": 1,
+            "parent_q": 4,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.127
+          },
+          {
+            "kid_q": 2,
+            "parent_q": 4,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.137
+          },
+          {
+            "kid_q": 3,
+            "parent_q": 4,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.213
+          },
+          {
+            "kid_q": 4,
+            "parent_q": 4,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.272
+          },
+          {
+            "kid_q": 5,
+            "parent_q": 4,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.25
+          }
+        ]
+      },
+      {
+        "key": "black_men",
+        "values": [
+          {
+            "kid_q": 1,
+            "parent_q": 4,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.271
+          },
+          {
+            "kid_q": 2,
+            "parent_q": 4,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.244
+          },
+          {
+            "kid_q": 3,
+            "parent_q": 4,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.226
+          },
+          {
+            "kid_q": 4,
+            "parent_q": 4,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.158
+          },
+          {
+            "kid_q": 5,
+            "parent_q": 4,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.1
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "key": "5",
+    "values": [
+      {
+        "key": "white_men",
+        "values": [
+          {
+            "kid_q": 1,
+            "parent_q": 5,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.1
+          },
+          {
+            "kid_q": 2,
+            "parent_q": 5,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.101
+          },
+          {
+            "kid_q": 3,
+            "parent_q": 5,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.162
+          },
+          {
+            "kid_q": 4,
+            "parent_q": 5,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.244
+          },
+          {
+            "kid_q": 5,
+            "parent_q": 5,
+            "income_type": "hh",
+            "key": "white_men",
+            "value": 0.393
+          }
+        ]
+      },
+      {
+        "key": "black_men",
+        "values": [
+          {
+            "kid_q": 1,
+            "parent_q": 5,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.215
+          },
+          {
+            "kid_q": 2,
+            "parent_q": 5,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.203
+          },
+          {
+            "kid_q": 3,
+            "parent_q": 5,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.218
+          },
+          {
+            "kid_q": 4,
+            "parent_q": 5,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.19
+          },
+          {
+            "kid_q": 5,
+            "parent_q": 5,
+            "income_type": "hh",
+            "key": "black_men",
+            "value": 0.174
+          }
+        ]
+      }
+    ]
+  }
+];
---
+++ index.md
@@ -0,0 +1,47 @@
+# Animated Data Graphic Re-creations
+
+*This page includes examples of animated data graphics to inform the design of the Data Animator system.*
+___
+
+## Examples:
+
+#### Newsroom Diversity
+
+* ["How Diverse Are US Newsrooms?" by Google News Initiative](https://googletrends.github.io/asne/?view=2&filter=gender)
+* Type: Keyframe Animation
+
+![Screen Capture](/img/01_newsroom_diversity.jpg "Newsroom Diversity Screen Capture")
+
+* __[Re-created Version](/01_newsroom_diversity/)__
+
+
+#### Baby Births
+
+* ["The Timing of Baby Making" by The Pudding](https://pudding.cool/2017/05/births/)
+* Type: Keyframe Animation / Preset Animation
+
+![Screen Capture](/img/02_baby_births.jpg "Newsroom Diversity Screen Capture")
+
+* __[Re-created Version](/02_baby_births/)__
+
+#### Intro to ML
+
+* ["A visual introduction to machine learning" by R2.D3](http://www.r2d3.us/visual-intro-to-machine-learning-part-1/)
+* Type: Procedural Animation
+
+![Screen Capture](/img/03_intro_to_ml.jpg "Newsroom Diversity Screen Capture")
+
+* __[Re-created Version](/03_intro_to_ml/)__
+
+#### Punishing Reach of Racism
+
+* ["Extensive Data Shows Punishing Reach of Racism for Black Boys" by New York Times](https://www.nytimes.com/interactive/2018/03/19/upshot/race-class-white-and-black-men.html)
+* Type: Procedural Animation
+
+![Screen Capture](/img/04_punishing_racism.jpg "Newsroom Diversity Screen Capture")
+
+* __[Re-created Version](/04_punishing_racism/)__
+
+___
+
+by John Thompson
